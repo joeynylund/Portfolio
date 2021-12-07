96c96f10b99067acab1a425d2112af98c3b88b9a
joeynylund <joeynylund@yahoo.com>
2021-12-06 20:20:36 -0500

    Added Moment

--- src/Git.js
+++ src/Git.js
@@ -3,7 +3,7 @@ import './App.css';
 import { Container, Row, Col } from 'reactstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faGithub } from '@fortawesome/free-brands-svg-icons';
-import { faExternalLinkAlt } from '@fortawesome/free-solid-svg-icons';
+import Moment from 'react-moment';
 
 function Git() {
 
@@ -31,6 +31,7 @@ function Git() {
                   <h2>{repo.name}</h2>
                   <h6>{repo.language}</h6>
                   <p>{repo.description}</p>
+                  <p>Updated <Moment fromNow>{repo.updated_at}</Moment></p>
                   <a href={repo.html_url} target="_blank"><FontAwesomeIcon icon={faGithub} size='2x' className="social-icon" /></a>
                 </div>
               </Col>
