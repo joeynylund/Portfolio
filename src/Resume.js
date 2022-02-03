99ee95ce7da2ffcb7b54d1bcc7871b71286d0081
joeynylund <joeynylund@yahoo.com>
2022-02-03 09:37:01 -0500

    Updated Resume link

--- src/Resume.js
+++ src/Resume.js
@@ -14,7 +14,7 @@ function Projects() {
             <h1 style={{padding:'20px'}}>Looking for my resume?</h1>
           </Col>
           <Col md="4">
-            <a href="https://docs.google.com/document/d/16Sa2RjjQ-UaG7hS2JVyWWuJhsxKe3Ez2oTYNGFPEVU8/edit?usp=sharing" target="_blank"><h1 className="resume-button" style={{textAlign:'center',backgroundColor:'#212121', padding:'20px', borderRadius:'10px',cursor:'pointer'}}>Here it is.</h1></a>
+            <a href="https://docs.google.com/document/d/1ki_8c8j8k5Y9HASN5bPrVi-iLGpxHOvr/edit?usp=sharing&ouid=102114095254430100841&rtpof=true&sd=true" target="_blank"><h1 className="resume-button" style={{textAlign:'center',backgroundColor:'#212121', padding:'20px', borderRadius:'10px',cursor:'pointer'}}>Here it is.</h1></a>
           </Col>
         </Row>
       </Container>
